a9b48ee2b4f9c9d8f422659c3657ea9ee0ff4fd6
julianmons442 <102389304+julianmons442@users.noreply.github.com>
2022-03-25 16:22:57 -0500

    Add files via upload

---
+++ CineC++.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int edad;
+	cout<<"Programa qie me permite ingresar a cine dependiendo la edad\n";
+	cout<<"Ingrese la edad: ";
+	cin>>edad;
+		if(edad>=18)
+	{
+		cout<<"Ingrese a su pelicula para adultos";
+	}
+			else
+			{
+				cout<<"Estas en la sala equivocada";
+			}
+	
+	
+	
+	
+	
+	
+}
